02e8b307814510ddbdd220a08ac76603700ee821
RedFoxFinn <laborantti.antti@gmail.com>
2025-11-24 15:15:57 +0200

    updated the setup script

--- setup.sh
+++ setup.sh
@@ -18,10 +18,10 @@ python3 -m venv venv
 source venv/bin/activate
 
 echo "Upgrading pip..."
-pip3 install --upgrade pip3
+pip install --upgrade pip
 
 echo "Installing requirements..."
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Running migrations..."
 python3 manage.py migrate
